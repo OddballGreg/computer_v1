9e9b189c65bac429d36e21c701d8f1e501ca95d6
Kalilo <kalilohansmann@gmail.com>
2017-10-19 07:06:51 +0200

    Added precision constant

--- math.rb
+++ math.rb
@@ -1,10 +1,12 @@
 class MyMath
+  PRECISION = 15
+
   def self.sqrt(x)
     a = 10
     100.times do
-      b = a.round(15)
+      b = a.round(PRECISION)
       a = a - (a * a - x) / (2 * a)
-      break if a.round(15) == b
+      break if a.round(PRECISION) == b
     end
     a
   end
